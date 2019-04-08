6e1458d4249e917c1199392139e51302d72ea6aa
TonyTandAndroid <tonytangandroid@gmail.com>
2019-04-08 08:48:26 -0700

    Add code to change the hello message.

--- app/src/main/java/com/example/hellobuck/MainActivity.java
+++ app/src/main/java/com/example/hellobuck/MainActivity.java
@@ -1,13 +1,19 @@
 package com.example.hellobuck;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView tv_hello = findViewById(R.id.tv_hello);
+        tv_hello.setText("Hello from Code");
+
     }
 }
